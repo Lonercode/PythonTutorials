f2f0bff9e5644d59dd14cfabbbcf1375671996ce
Lonercode <91500485+Lonercode@users.noreply.github.com>
2022-10-26 21:30:43 +0100

    Create start.py

---
+++ start.py
@@ -0,0 +1,10 @@
+
+"""
+Hello :)
+
+This is just a sample file, nothing fancy.
+I hope you enjoy your time exploring git.
+In this repository we will collaborate on 
+the brilliant code you write.
+
+"""
